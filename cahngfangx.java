d235dda042848bc6fbe5bc7ae57c078bd4b3d0be
ZCzGT <2513597003@qq.com>
2023-12-09 05:25:31 +0000

    update cahngfangx.java.
    Signed-off-by: ZCzGT <2513597003@qq.com>

--- cahngfangx.java
+++ cahngfangx.java
@@ -8,15 +8,10 @@ public class cahngfangx {
         System.out.println("请输入宽(整数)");
         int W = scanner.nextInt();
         for (int i = 0; i < H; i++) {
-
             for (int j = 0; j < W; j++) {
-
                 System.out.print("*");
             }
            System.out.print("\n");
-
-
         }
-
     }
 }
